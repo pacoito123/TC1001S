b23d44db6975c463a5958f7607b9e740bf7978cf
pacoito123 <pacoito@protonmail.com>
2022-03-23 15:30:12 -0600

    Implementación 'cube()'; unos ajustes. - Se creó un '.gitignore' para el repositorio. - Se cambió el nombre de la ventana del programa. - Se redujo el número de pasos de 'circle()' a 36, de 360. - El método 'triangle()' ahora sigue la dirección en la que se trazó la figura.

--- Act1/paint.py
+++ Act1/paint.py
@@ -51,19 +51,50 @@ def square(start, end):
     setheading(0)
 
 
+def cube(start, end):
+    up()
+    goto(start.x, start.y) # Comenzar a trazar en la posición inicial.
+    down()
+
+    dist = math.sqrt((end.x - start.x)**2 + (end.y - start.y)**2) # Obtener distancia entre los dos puntos.
+    theta = math.degrees(math.atan2(end.y - start.y, end.x - start.x)) # Obtener ángulo entre los dos puntos.
+    direccion = -1 if abs(theta) >= 90 else 1 # Determinar dirección del trazado.
+
+    setheading(theta)
+
+    for _ in range(4): # Trazar cuadrado inicial.
+        forward(dist)
+        right(90 * direccion)
+
+    left(45 * direccion)
+    forward(dist / 2.0) # Conectar primera vértice.
+    right(45 * direccion)
+
+    for _ in range(4):
+        curr = heading()
+        forward(dist) # Trazar siguiente lado.
+
+        setheading(theta - 135 * direccion)
+        forward(dist / 2.0) # Conectar siguiente vértice.
+        forward(-dist / 2.0)
+        setheading(curr - 90 * direccion)
+
+    setheading(0)
+
+
 def circle(start, end):
     """Draw circle from start to end."""
     up()
 
     mid = vector((start.x + end.x) / 2.0, (start.y + end.y) / 2.0) # Obtener punto medio.
-    radius = math.sqrt((end.x - start.x) ** 2 + (end.y - start.y) ** 2) / 2.0 # Calcular radio.
+    radius = math.sqrt((end.x - start.x)**2 + (end.y - start.y)**2) / 2.0 # Calcular radio.
 
     goto(radius + mid.x, mid.y) # Comenzar a trazar en la primera posición.
 
     down()
     begin_fill()
 
-    for i in range(1, 360):
+    for i in range(1, 360, 10):
         if i not in cache.keys(): # 'Memoization' para funciones trigonométricas.
             cache[i] = [math.cos(math.radians(i)), math.sin(math.radians(i))]
 
@@ -103,11 +134,20 @@ def triangle(start, end):
     down()
     begin_fill()
 
-    for count in range(3):
-        forward(end.x - start.x)
-        left(120)
+    dist = math.sqrt((end.x - start.x)**2 + (end.y - start.y)**2)
+    theta = math.degrees(math.atan2(end.y - start.y, end.x - start.x))
+    setheading(theta)
+
+    for _ in range(3):
+        forward(dist)
+
+        if abs(theta) >= 90:
+            left(120)
+        else:
+            right(120)
 
     end_fill()
+    setheading(0)
 
 
 def tap(x, y):
@@ -127,6 +167,7 @@ def store(key, value):
     """Store value in state at key."""
     state[key] = value
 
+title("Equipo 6: Act 1 - paint.py") # Nombre de la ventana
 speed(10000) # Velocidad del cursor.
 
 state = {'start': None, 'shape': line}
@@ -142,6 +183,7 @@ onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
+onkey(lambda: store('shape', cube), 'C') # Agregamos función para trazar un cubo.
 onkey(lambda: store('shape', circle), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
