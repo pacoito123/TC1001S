a42302739e765b5adb98944242923e40b977f0a6
pacoito123 <pacoito@protonmail.com>
2022-03-22 18:03:23 -0600

    Implementación 'circle()'; un par de correcciones. - Método 'circle()' implementado, con memoization (aunque no es tan necesario). - Método 'square()' ahora sigue la dirección en la que se trazó la figura. - Método 'rectangle()' ahora sigue la dirección en la que se trazó la figura.

--- Act1/paint.py
+++ Act1/paint.py
@@ -1,4 +1,5 @@
-"""Paint, for drawing shapes.
+"""
+Paint, for drawing shapes.
 
 Exercises
 
@@ -12,6 +13,11 @@ Exercises
 from turtle import *
 
 from freegames import vector
+
+import math
+
+
+cache = {} # Para 'memoization'.
 
 
 def line(start, end):
@@ -29,33 +35,63 @@ def square(start, end):
     down()
     begin_fill()
 
-    for count in range(4):
-        forward(end.x - start.x)
-        left(90)
+    dist = math.sqrt((end.x - start.x)**2 + (end.y - start.y)**2)
+    theta = math.degrees(math.atan2(end.y - start.y, end.x - start.x))
+    setheading(theta)
+
+    for _ in range(4):
+        forward(dist)
+
+        if abs(theta) >= 90:
+            left(90)
+        else:
+            right(90)
 
     end_fill()
+    setheading(0)
 
 
 def circle(start, end):
     """Draw circle from start to end."""
-    pass  # TODO
-
-
-def rectangle(start, end):
-    #se uso la misma funcion que el cuadrado solo que a los lados verticales los hicimos mas cortos, dividiendolos entre 2 de modo que dos pares de lados son iguales pero diferentes entre el otro par
     up()
-    goto(start.x, start.y)
+
+    mid = vector((start.x + end.x) / 2.0, (start.y + end.y) / 2.0) # Obtener punto medio.
+    radius = math.sqrt((end.x - start.x) ** 2 + (end.y - start.y) ** 2) / 2.0 # Calcular radio.
+
+    goto(radius + mid.x, mid.y) # Comenzar a trazar en la primera posición.
+
     down()
     begin_fill()
-    
-    for count in range(4):
-        if count%2 == 0:
+
+    for i in range(1, 360):
+        if i not in cache.keys(): # 'Memoization' para funciones trigonométricas.
+            cache[i] = [math.cos(math.radians(i)), math.sin(math.radians(i))]
+
+        goto(radius * cache[i][0] + mid.x, radius * cache[i][1] + mid.y) # Ir a la siguiente posición.
+
+    end_fill()
+
+
+"""
+    Se usó el mismo concepto que en la función 'cuadrado()', solo que cada número avanza a una
+    distancia diferente (Δx o Δy) dependiendo si es par o impar.
+"""
+def rectangle(start, end):
+    """Draw rectangle from start to end."""
+    up()
+
+    goto(start.x, start.y)
+
+    down()
+    begin_fill()
+
+    for i in range(4):
+        if i % 2 == 0:
             forward(end.x - start.x)
             left(90)
         else:
-            forward((end.x - start.x)/2)
+            forward((end.y - start.y))
             left(90)
-       
 
     end_fill()
 
@@ -82,6 +118,7 @@ def store(key, value):
     """Store value in state at key."""
     state[key] = value
 
+speed(10000) # Velocidad del cursor.
 
 state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
@@ -91,7 +128,7 @@ onkey(undo, 'u')
 onkey(lambda: color('black'), 'K')
 onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
-onkey(lambda: color('pink'), 'P') #Agregamos color de la lista de colores de python
+onkey(lambda: color('pink'), 'P') # Agregamos color de la lista de colores del programa.
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
 onkey(lambda: store('shape', line), 'l')
--- README.md
+++ README.md
@@ -6,10 +6,10 @@ Repositorio con proyectos y actividades en Python para este taller de programaci
 
 ## Autores
 
-- **Cesar Hernandez** -[Zzar012345](https://github.com/Zzar012345)
-- **Guillermo Garrido** -[Rockking01](https://github.com/Rockking01)
-- **Francisco Rubio** - [pacoito123](https://github.com/pacoito123)
-- **Genaro Rivero-A00833638** -[Alponcho6594](https://github.com/Alponcho6594)
+- **Cesar Hernandez** - [Zzar012345](https://github.com/Zzar012345)
+- **Guillermo Garrido** - [Rockking01](https://github.com/Rockking01)
+- **Francisco Rubio (A01284502)** - [pacoito123](https://github.com/pacoito123)
+- **Genaro Rivero (A00833638)** - [Alponcho6594](https://github.com/Alponcho6594)
 
 ## Licencia
 
