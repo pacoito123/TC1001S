3b7ac65052feae8d327a6a4986e7803dc5b82f42
pacoito123 <pacoito@protonmail.com>
2022-03-24 12:15:55 -0600

    Cambios iniciales del juego 'memory.py'. - Se agregó un contador de 'taps'. - Ahora se ignoran 'taps' afuera de la cuadrícula. - Se alinearon los dígitos al centro de la casilla. - Se añadió una constante para modificar el tamaño de la cuadrícula. - Se cambió el modo de color a '255'. - Se comenzaron a implementar colores para recordar casillas mejor.

--- Act5/memory.py
+++ Act5/memory.py
@@ -1,4 +1,5 @@
-"""Memory, puzzle game of number pairs.
+"""
+Memory, puzzle game of number pairs.
 
 Exercises:
 
@@ -14,10 +15,21 @@ from turtle import *
 
 from freegames import path
 
+import math
+
+
+# Se implementó esta constante para cambiar el número de casillas.
+num_tiles = 8 # TODO: Arreglar con números impares.
+
 car = path('car.gif')
-tiles = list(range(32)) * 2
+tiles = list(range(math.ceil(num_tiles**2 / 2.0))) * 2
 state = {'mark': None}
-hide = [True] * 64
+hide = [True] * (num_tiles**2)
+
+# Contador(es)
+counter_turtle = Turtle() # Crear instancia nueva de 'Turtle()'.
+counter = counter_last = 0 # Inicializar en 0.
+# ...
 
 
 def square(x, y):
@@ -35,16 +47,22 @@ def square(x, y):
 
 def index(x, y):
     """Convert (x, y) coordinates to tiles index."""
-    return int((x + 200) // 50 + ((y + 200) // 50) * 8)
+    half = num_tiles / 2.0 # Calcular la mitad del número de casillas.
+    return int((x + (50 * half)) // 50 + ((y + (50 * half)) // 50) * num_tiles)
 
 
 def xy(count):
     """Convert tiles count to (x, y) coordinates."""
-    return (count % 8) * 50 - 200, (count // 8) * 50 - 200
+    half = num_tiles / 2.0 # Calcular la mitad del número de casillas.
+    return (count % num_tiles) * 50 - (50 * half), (count // num_tiles) * 50 - (50 * half)
 
 
 def tap(x, y):
     """Update mark and hidden tiles based on tap."""
+    half = num_tiles / 2.0 # Calcular la mitad del número de casillas.
+    if x > 50 * half or x < -50 * half or y > 50 * half or y < -50 * half:
+        return # Ignorar 'taps' fuera de la cuadrícula.
+
     spot = index(x, y)
     mark = state['mark']
 
@@ -54,6 +72,10 @@ def tap(x, y):
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
+        # TODO: Añadir condición de salida cuando se agoten los números.
+
+    global counter
+    counter += 1 # Aumentar contador de 'taps'.
 
 
 def draw():
@@ -63,7 +85,7 @@ def draw():
     shape(car)
     stamp()
 
-    for count in range(64):
+    for count in range(num_tiles**2):
         if hide[count]:
             x, y = xy(count)
             square(x, y)
@@ -73,13 +95,41 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
-        color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
+        goto(x + 25, y) # Se cambió de '+ 2' a '+ 25' para alinear valor al centro.
+        color((tiles[mark] * 10 % 255, 128, tiles[mark] * 10 % 255))
+
+        print(tiles[mark] * 10 % 255, ", ", 128, ", ", tiles[mark] * 10 % 255)
+        write(tiles[mark], font=('Arial', 30, 'normal'), align='center') # Se añadió 'align' para alinear valor al centro.
 
     update()
+    updateCounter() # Actualizar contador.
     ontimer(draw, 100)
 
+
+"""
+    Función para actualizar el contador de taps.
+
+    Se utilizó otra instancia de la clase 'Turtle()' para evitar el parpadeo del número a causa
+    de la tortuga original teniendo que ir al otro lado de la ventana para desplegar el número.
+"""
+def updateCounter():
+    global counter_last
+
+    if counter != counter_last: # Para únicamente desplegar el contador cuando haya un cambio.
+        counter_turtle.clear() # Limpiar pantalla.
+        counter_turtle.write('Taps: ' + str(counter), font=('Arial', 30, 'normal')) # Desplegar 'taps'.
+
+        counter_last = counter # Actualizar último valor del contador.
+
+
+# Configuración de tortuga para contador:
+counter_turtle.hideturtle()
+counter_turtle.up()
+counter_turtle.goto(-50 * (num_tiles / 2.0 + 1), -50 * (num_tiles / 2.0 + 1))
+counter_turtle.down()
+# ...
+
+colormode(255) # Cambiar color a 0-255, en lugar de 0..1 (por default).
 
 shuffle(tiles)
 setup(420, 420, 370, 0)
