fe46e77167758b2fa851433eb5c80ce6f2860551
pacoito123 <pacoito@protonmail.com>
2022-03-25 15:21:47 -0600

    Últimas correcciones/ajustes del juego de Pacman. - Se cambió la velocidad de los fantasmas a 10 (de 20). - Inputs menos precisos ahora son tomados en cuenta al dar vueltas. - Se modificó el contador de puntaje; ahora también muestra el total de puntos. - Se agregó una función para pausar y salir de juego. - Se modificó el README.md para reflejar estos nuevos cambios.

--- Act3/README.md
+++ Act3/README.md
@@ -4,18 +4,58 @@
 
 Programa sencillo para simular el juego de Pacman, cambiamos el mapa, la velocidad de los fantasmas y la forma en que los fantasmas buscan al usuario '[turtle, random](https://docs.python.org/3/library/turtle.html)'.
 
-## Controladores
+## Controles
+*Mover a Pacman:*
+* **[↑]** - Mover arriba
+* **[↓]** - Mover abajo
+* **[←]** - Mover a la izquierda
+* **[→]** - Mover a la derecha
 
-*Nuevas Funciones:*
-* **[tiles = [1,0,1,0,1,0]]** - Creación del mapa con un nuevo camino
-* **[options = [**
-                **vector(20, 0),**
-                **vector(-20, 0),**
-                **vector(0, 20),**
-                **vector(0, -20),]** - Incremento en la velocidad
-* **[plan = choice(options)]** - Cambio de posiciones más rapido
-* **[if valid(point + course):**
-            **point.move(course)]** - Fantasmas buscando al jugador
+[========]
+
+*Juego:*
+* **[p]** - Pausar/continuar juego
+* **[q]** - Salir del juego
+
+## Nuevas Funciones
+* **Creación del mapa con un nuevo camino:**
+```python
+tiles = [1,0,1,0,1,0]
+```
+* **Incremento en la velocidad de los fantasmas:**
+```python
+options = [
+	vector(10, 0),
+	vector(-10, 0),
+	vector(0, 10),
+	vector(0, -10),
+]
+```
+* **Decisión de movimiento de los fantasmas:**
+```python
+plan = choice(options)
+```
+* **Movimiento de los fantasmas:**
+```python
+if valid(point + course, 0):
+	point.move(course)
+```
+* **Permitir inputs menos precisos:**
+```python
+# método change()
+if valid(pacman + vector(x, y), 20):
+        new_aim.x = x
+        new_aim.y = y
+# método move()
+if not aim == new_aim and valid(pacman + new_aim, 0):
+        aim.x = new_aim.x
+        aim.y = new_aim.y
+        pacman.move(aim)
+```
+* **Mostrar total de puntos a obtener:**
+```python
+total_tiles = sum(tiles)
+```
 
 [========]
 
--- Act3/pacman.py
+++ Act3/pacman.py
@@ -1,11 +1,15 @@
 from random import choice
 from turtle import *
+
 from freegames import floor, vector
 
-state = {'score': 0}
+import math
+
+state = {'score': 0, 'pause': False}
 path = Turtle(visible=False)
 writer = Turtle(visible=False)
 aim = vector(5, 0)
+new_aim = vector(5, 0)
 pacman = vector(-40, -80)
 ghosts = [
     [vector(-180, 160), vector(5, 0)],
@@ -35,10 +39,11 @@ tiles = [
     0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-]
-#Cambio del mapa, modificando los 0's y 1's de la  matriz para crear un nuevo diseño
+] # Cambio del mapa, modificando los 0's y 1's de la  matriz para crear un nuevo diseño
+total_tiles = sum(tiles) # Total de puntos a obtener.
 
-def square(x, y): #Funcion que crea el tablero
+
+def square(x, y): # Función que crea el tablero
     "Draw square using path at (x, y)."
     path.up()
     path.goto(x, y)
@@ -51,14 +56,16 @@ def square(x, y): #Funcion que crea el tablero
 
     path.end_fill()
 
-def offset(point): #Funcion que crea el piso 
+
+def offset(point): # Función para determinar un ajuste en posición.
     "Return offset of point in tiles."
     x = (floor(point.x, 20) + 200) / 20
     y = (180 - floor(point.y, 20)) / 20
     index = int(x + y * 20)
     return index
 
-def valid(point): #Funcion valid que valida si es permitido el movimiento
+
+def valid(point, tolerance): # Función que valida si es permitido el movimiento
     "Return True if point is valid in tiles."
     index = offset(point)
 
@@ -70,9 +77,10 @@ def valid(point): #Funcion valid que valida si es permitido el movimiento
     if tiles[index] == 0:
         return False
 
-    return point.x % 20 == 0 or point.y % 20 == 0
+    return abs(point.x) % 20 <= tolerance or abs(point.y) % 20 <= tolerance # Permitir cierta tolerancia al dar vueltas.
 
-def world(): #Funcion world que crea el mundo interno del mapa
+
+def world(): # Función que crea el mundo interno del mapa
     "Draw world using path."
     bgcolor('black')
     path.color('blue')
@@ -90,19 +98,31 @@ def world(): #Funcion world que crea el mundo interno del mapa
                 path.goto(x + 10, y + 10)
                 path.dot(2, 'white')
 
-def move(): #Funcion move que genera el movimiento de los objetos
+
+def move(): # Función que genera el movimiento de los objetos.
     "Move pacman and all ghosts."
+    if state['pause']:
+        ontimer(move, 10)
+        return
+
     writer.undo()
-    writer.write(state['score'])
+    writer.write(str(state['score']) + ' / ' + str(total_tiles), # Mostrar total de puntos a obtener.
+        font=('Consolas', 24, 'normal'), align='center') # Usar otra font y alinear al centro.
 
     clear()
 
-    if valid(pacman + aim):
+    if not aim == new_aim and valid(pacman + new_aim, 0): # Esperar a que esté alineado para dar una vuelta.
+        aim.x = new_aim.x
+        aim.y = new_aim.y
+
+        pacman.move(aim) # Mover a Pacman un poco más rápido al dar vueltas.
+
+    if valid(pacman + aim, 0): # Mover a Pacman si su siguiente movimiento es válido.
         pacman.move(aim)
 
-    index = offset(pacman)
+    index = offset(pacman) # Obtener posición de Pacman
 
-    if tiles[index] == 1:
+    if tiles[index] == 1: # Recolectar punto.
         tiles[index] = 2
         state['score'] += 1
         x = (index % 20) * 20 - 200
@@ -113,17 +133,17 @@ def move(): #Funcion move que genera el movimiento de los objetos
     goto(pacman.x + 10, pacman.y + 10)
     dot(20, 'yellow')
 
-    for point, course in ghosts:
-        if valid(point + course):
+    for point, course in ghosts: # Calcular movimiento de cada fantasma.
+        if valid(point + course, 0):
             point.move(course)
         else:
             options = [
-                vector(20, 0),
-                vector(-20, 0),
-                vector(0, 20),
-                vector(0, -20),
+                vector(10, 0),
+                vector(-10, 0),
+                vector(0, 10),
+                vector(0, -10),
             ]
-            plan = choice(options) #Modificando el vector donde los fantasmas se pueden mover puedes hacer a los fantas mas rapidos/listos
+            plan = choice(options) # Modificando el vector donde los fantasmas se pueden mover puedes hacer a los fantasmas más rápidos.
             course.x = plan.x
             course.y = plan.y
 
@@ -133,22 +153,28 @@ def move(): #Funcion move que genera el movimiento de los objetos
 
     update()
 
-    for point, course in ghosts:
+    for point, course in ghosts: # Calcular colisión con Pacman.
         if abs(pacman - point) < 20:
             return
 
-    ontimer(move, 10) #Cambio de Tiempo de movimiento donde el segundo argumento es t y a mayor t menor velocidad y visceversa
+    ontimer(move, 10) # Cambio de tiempo de movimiento, donde el segundo argumento es t y a mayor t menor velocidad y visceversa.
 
-def change(x, y): #Funcion change que cambia el curso del pacman 
+
+def change(x, y): # Función que cambia el curso del jugador.
     "Change pacman aim if valid."
-    if valid(pacman + vector(x, y)):
-        aim.x = x
-        aim.y = y
+    if valid(pacman + vector(x, y), 20):
+        new_aim.x = x
+        new_aim.y = y
+
+
+def toggle_pause(): # Función para cambiar el estado de pausa.
+    state['pause'] = not state['pause']
+
 
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
-writer.goto(160, 160)
+writer.goto(0, 200)
 writer.color('white')
 writer.write(state['score'])
 listen()
@@ -156,6 +182,8 @@ onkey(lambda: change(5, 0), 'Right')
 onkey(lambda: change(-5, 0), 'Left')
 onkey(lambda: change(0, 5), 'Up')
 onkey(lambda: change(0, -5), 'Down')
+onkey(lambda: toggle_pause(), 'p') # Pausar/continuar jugando.
+onkey(lambda: exit(), 'q') # Salir del juego.
 world()
 move()
 done()
